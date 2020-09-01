77722593003fc9e19e06a449198fdb3ae5fffbff
diogoshiroma <diogoshiroma@hotmail.com>
2020-09-01 20:06:44 -0300

    Add lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./App.css";
-import { Home } from "./Home";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+import { Home } from './Home';
 
-function App() {
+const App = () => {
   return (
     <React.Fragment>
       <Router>
         <Switch>
-          <Route exact path="/" component={Home} />
+          <Route exact path={'/'} component={Home} />
         </Switch>
       </Router>
     </React.Fragment>
   );
-}
+};
 
 export default App;
