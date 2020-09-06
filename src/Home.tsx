4f268379e834fe7fefb914cf3a46ac0ee7590def
diogoshiroma <diogoshiroma@hotmail.com>
2020-09-05 22:57:32 -0300

    Add navbar button component

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import './components/Navbar.css';
+import { NavButton } from './components/NavButton';
 
 export const Home = () => {
   return (
-    <div>
+    <div style={{ backgroundColor: '#021727' }}>
+      <NavButton
+        backgroundColor={'#007CF9'}
+        textColor={'white'}
+        lineHeight={'25px'}
+        border={'2px solid rgba(0, 0, 0, 0.25)'}
+      >
+        Say hello
+      </NavButton>
       <h2>Teste</h2>
     </div>
   );
---
+++ src/components/NavButton.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface NavButtonProps {
+  textColor?: string;
+  backgroundColor?: string;
+  fontSize?: string;
+  fontWeight?: string;
+  padding?: string;
+  border?: string;
+  cursor?: string;
+  lineHeight?: string;
+  children: any;
+}
+
+const StyledButton = styled.button`
+  border: none;
+  background-color: transparent;
+  padding: 8px 38px;
+  cursor: pointer;
+
+  ${(props: NavButtonProps) =>
+    props &&
+    `
+      color: ${props.textColor};
+      border: ${props.border};
+      textColor: ${props.textColor};
+      background-color: ${props.backgroundColor};
+      font-weight: ${props.fontWeight};
+      font-size: ${props.fontSize}px;
+      padding: ${props.padding};
+      cursor: ${props.cursor};
+      line-height: ${props.lineHeight};
+    `}
+`;
+
+export const NavButton = (props: NavButtonProps) => {
+  return <StyledButton {...props} />;
+};
