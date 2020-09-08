436a781bb42f41b0d7ff37e22b55811d3eebe2ef
diogoshiroma <diogoshiroma@hotmail.com>
2020-09-08 12:08:12 -0300

    Add react slick and first carousel sketch

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './components/Navbar.css';
+import { Carousel } from './components/Carousel';
 
 export const Home = () => {
   return (
-    <div style={{ backgroundColor: '#021727' }}>
+    <div
+      style={{
+        backgroundColor: '#021727',
+      }}
+    >
       <h2>Teste</h2>
+      <Carousel />
     </div>
   );
 };
---
+++ src/components/Carousel.tsx
@@ -0,0 +1,131 @@
+import React from 'react';
+import Slider from 'react-slick';
+import styled from 'styled-components';
+
+// function SampleNextArrow(props: any) {
+//   const { className, style, onClick } = props;
+//   return (
+//     <div
+//       onClick={onClick}
+//       className={className}
+//       style={{
+//         // ...style,
+//         // display: 'block',
+//         // position: 'absolute',
+//         // right: '-25px',
+//         // top: '48%',
+//         // backgroundColor: 'red',
+//         // textAlign: 'center',
+//         // width: '20px',
+//         // height: '20px',
+
+//         ...style,
+//         display: 'block',
+//         background: 'red',
+//       }}
+//     />
+//   );
+// }
+
+interface DivProps {
+  backgroundColor?: string;
+  padding?: string;
+  margin?: string;
+  height?: string;
+  width?: string;
+  position?: string;
+  transform?: string;
+  top?: string;
+  left?: string;
+}
+
+const StyledDiv = styled.div`
+  background-color: red;
+  height: 300px;
+  position: relative;
+
+  ${(props: DivProps) =>
+    props &&
+    `
+      background-color: ${props.backgroundColor};
+      padding: ${props.padding};
+      margin: ${props.margin};
+      height: ${props.height};
+      width: ${props.width};
+      position: ${props.position};
+      transform: ${props.transform};
+      top: ${props.top};
+      left: ${props.left};
+    `}
+`;
+
+const StyledCard = styled.div`
+  background-color: gray;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  width: 85%;
+  transform: translate(-50%, -50%);
+
+  ${(props: DivProps) =>
+    `
+        background-color: ${props.backgroundColor};
+    `}
+`;
+
+export const Carousel = () => {
+  const settings = {
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <div
+      style={{
+        padding: '50px',
+      }}
+    >
+      <Slider {...settings}>
+        <StyledDiv>
+          <StyledCard>
+            <h1 style={{ textAlign: 'center' }}>potato</h1>
+          </StyledCard>
+        </StyledDiv>
+        <StyledDiv backgroundColor={'blue'}>
+          <StyledCard>
+            <h1 style={{ textAlign: 'center' }}>potato</h1>
+          </StyledCard>
+        </StyledDiv>
+        <StyledDiv backgroundColor={'green'}>
+          <StyledCard>
+            <h1 style={{ textAlign: 'center' }}>potato</h1>
+          </StyledCard>
+        </StyledDiv>
+        <StyledDiv backgroundColor={'yellow'}>
+          <StyledCard>
+            <h1 style={{ textAlign: 'center' }}>potato</h1>
+          </StyledCard>
+        </StyledDiv>
+      </Slider>
+    </div>
+  );
+};
