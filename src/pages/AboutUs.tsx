7265b0c4c1e3c4d4dba048e4b97c3867c81533b7
diogoshiroma <diogoshiroma@hotmail.com>
2020-09-22 18:59:13 -0300

    Add two column content

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -8,16 +8,21 @@ import ProductCanvasPhoto from 'assets/images/ProductCanvasPhoto.png';
 import ProductCanvasZoomed from 'assets/images/ProductCanvasZoomed.png';
 import WritingWallPostitPhoto from 'assets/images/WritingWallPostitPhoto.png';
 import WritingWallPostitZoomed from 'assets/images/WritingWallPostitZoomed.png';
+import { Col, Row } from 'react-flexbox-grid';
 
 const StyledHeader = styled.div`
   margin: 80px 288px 37px;
   text-align: center;
 
   @media (max-width: 1209px) {
-    margin: 80px 240px 37px;
+    margin: 80px 200px 37px;
   }
 
-  @media (max-width: 898px) {
+  @media (max-width: 950px) {
+    margin: 80px 100px 37px;
+  }
+
+  @media (max-width: 767px) {
     margin: 80px 16px 37px;
   }
 `;
@@ -26,7 +31,7 @@ const StyledHeaderTitle = styled.div`
   font-size: 88px;
   font-weight: bold;
 
-  @media (max-width: 898px) {
+  @media (max-width: 767px) {
     font-size: 40px;
   }
 `;
@@ -35,7 +40,7 @@ const StyledHeaderText = styled.div`
   font-size: 36px;
   padding-top: 32px;
 
-  @media (max-width: 898px) {
+  @media (max-width: 767px) {
     font-size: 24px;
   }
 `;
@@ -120,6 +125,49 @@ const StyledWritingWallPostitZoomed = styled.img`
   }
 `;
 
+const StyledContent = styled.div`
+  margin: 80px 64px;
+
+  @media (max-width: 898px) {
+    margin: 40px 16px;
+  }
+`;
+
+const StyledContentTitle = styled.div`
+  font-size: 88px;
+  font-weight: bold;
+
+  @media (max-width: 767px) {
+    font-size: 40px;
+  }
+`;
+
+const StyledContentText = styled.div`
+  font-size: 32px;
+  margin-top: 24px;
+  margin-right: 100px;
+
+  @media (max-width: 767px) {
+    margin-right: 0;
+    font-size: 24px;
+  }
+`;
+
+const StyledCardWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const StyledCard = styled.div`
+  border-radius: 8px;
+  margin-top: 136px;
+  background-color: blue;
+  font-size: 88px;
+  height: 274px;
+  width: 560px;
+  text-align: center;
+`;
+
 const AboutUs = () => {
   return (
     <>
@@ -167,6 +215,25 @@ const AboutUs = () => {
           alt={'writing-wall-postit-zoomed'}
         />
       </StyledPhotosRow>
+
+      <StyledContent>
+        <Row center={'xs'} start={'md'}>
+          <Col sm={12} md={6}>
+            <StyledContentTitle>Excellence</StyledContentTitle>
+            <StyledContentText>
+              We aim high, and we want to evolve every day, so it is mandatory
+              to learn continuously and try new innovative ideas with strong
+              discipline. We value those who give their best in their jobs and
+              genuinely love what they do. When you love what you do, you excel.
+            </StyledContentText>
+          </Col>
+          <Col md={6} className={'hidden-xs hidden-sm'}>
+            <StyledCardWrapper>
+              <StyledCard>Potato</StyledCard>
+            </StyledCardWrapper>
+          </Col>
+        </Row>
+      </StyledContent>
     </>
   );
 };
